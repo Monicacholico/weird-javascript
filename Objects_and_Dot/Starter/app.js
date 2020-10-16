199ce20fd899546add1a200046acd17e93c67675
Monicacholico <monicachol@hotmail.com>
2020-10-15 23:20:58 -0500

    chapter 30

---
+++ Objects_and_Dot/Starter/app.js
@@ -0,0 +1,21 @@
+var person = new Object();
+
+person["firstName"] = "Tony";
+person["lastName"] = "Alicea";
+
+var firstNameProperty = "firstName";
+
+console.log(person);
+console.log(person[firstNameProperty]);
+
+console.log(person.firstName);
+console.log(person.lastName);
+
+person.address = new Object();
+person.address.street = "111 Main St";
+person.address.city = "New York";
+person.address.state = "NY";
+
+console.log(person.address.street);
+console.log(person.address.city);
+console.log(person["address"]["state"]);
